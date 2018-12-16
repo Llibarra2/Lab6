f44900b4fb829723a34afdeeaeae7ed7e406e29b
Llibarra2 <43329982+Llibarra2@users.noreply.github.com>
2018-12-16 06:30:22 -0700

    Update Lab6B.py

--- Lab6B.py
+++ Lab6B.py
@@ -10,17 +10,14 @@ class Graph:
   
     def __init__(self,vertices): 
         self.vertex= vertices #No. of vertices 
-        self.graph = [] # default dictionary  
-                                # to store graph 
+        self.graph = [] # default graph storage
           
    
-    # function to add an edge to graph 
-    def addEdge(self,u,v,w): 
+    def addEdge(self,u,v,w):    # function to add an edge to graph 
         self.graph.append([u,v,w]) 
   
-    # A utility function to find set of an element i 
-    # (uses path compression technique) 
-    def find(self, parent, i): 
+    
+    def find(self, parent, i): # A utility function to find set of an element i 
         if parent[i] == i: 
             return i 
         return self.find(parent, parent[i]) 
@@ -29,35 +26,26 @@ class Graph:
         xroot = self.find(parent, x) 
         yroot = self.find(parent, y) 
   
-        # Attach smaller rank tree under root of  
-        # high rank tree (Union by Rank) 
-        if rank[xroot] < rank[yroot]: 
+        if rank[xroot] < rank[yroot]:         # Attach smaller rank tree under root of   high rank tree (Union by Rank) 
             parent[xroot] = yroot 
         elif rank[xroot] > rank[yroot]: 
             parent[yroot] = xroot 
   
-        # If ranks are same, then make one as root  
-        # and increment its rank by one 
-        else : 
+        else :        # If ranks are same, then make one as root and increment its rank by one 
             parent[yroot] = xroot 
             rank[xroot] += 1
   
-    # The main function to construct MST using Kruskal's  
-        # algorithm 
-    def Kruskals(self): 
+    def Kruskals(self):  # The main function to construct MST using Kruskal's  
   
         result =[] #This will store the resultant MST 
   
         edge = 0 # An index variable, used for sorted edges 
         index = 0 # An index variable, used for result[] 
   
-            # Step 1:  Sort all the edges in non-decreasing  
-                # order of their 
-                # weight.  If we are not allowed to change the  
-                # given graph, we can create a copy of graph 
         self.graph =  sorted(self.graph,key=lambda item: item[2]) 
   
-        parent = [] ; rank = [] 
+        parent = [] 
+        rank = [] 
   
         # Create V subsets with single elements 
         for node in range(self.vertex): 
@@ -83,7 +71,7 @@ class Graph:
   
 def main():# Driver code 
     test = Graph(5) 
-    test.addEdge(0, 1, 7) 
+    test.addEdge(0, 1, 7)#the method in which information is used to create an edege using a source, destination, weight
     test.addEdge(0, 2, 3) 
     test.addEdge(0, 3, 5) 
     test.addEdge(1, 3, 13) 
